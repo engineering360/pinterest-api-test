6ab5e34ae54885ea16c7a4801054d19e9340accc
mariapesm <youandmecanyousee@hotmail.com>
2019-01-04 19:32:05 +0200

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AllPosts from './AllPosts'
+
+const Home = () => (
+  <div>
+    <div className="home--container">
+      <div className="allposts--container">
+        <AllPosts/>
+      </div>
+    </div>
+  </div>
+)
+
+export default Home;
