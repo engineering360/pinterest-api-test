e91dbab9aaef3333f6d472664c40bd1bb62b370f
mariapesm <youandmecanyousee@hotmail.com>
2019-01-04 19:18:32 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Profile from "./components/Profile";
+import PostView from "./components/PostView";
+
+const client = new ApolloClient({
+  link: new HttpLink({ 
+    uri: "/graphql", 
+    credentials: 'same-origin'
+  }),
+  cache: new InMemoryCache()
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <div>
+            <Nav/>
+            <div className="site--container">
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/about" component={About}/>
+              <Route path="/profile/:id" component={Profile}/>
+              <Route path="/post/:id" component={PostView}/>
+            </div>
+            <Footer/>
+          </div>
+        </BrowserRouter>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
