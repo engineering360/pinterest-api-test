071be75e75654c1780038add75232056f2670377
mariapesm <youandmecanyousee@hotmail.com>
2019-01-04 19:19:46 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/reset.css';
+import './styles/index.css';
+import './styles/masonry.css';
+import './styles/spinner.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
